50e1e5f4e6b848742c508f73b1b505be9229360e
Shevek <gtophuria@gmail.com>
2021-03-12 23:33:47 +0400

    :lipstick: added header styles and dark/light mode icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,58 @@
 import React from "react";
 import useDarkMode from "../hooks/useDarkMode";
 
+// icons
+import Moon from "../assets/icons/Moon";
+import Sun from "../assets/icons/Sun";
+
 const Header = () => {
   const [colorTheme, setTheme] = useDarkMode();
   return (
-    <header className="text-gray-600 body-font dark:bg-black">
-      <div className="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center">
+    <header className="dark:bg-black">
+      <div className="mx-auto flex flex-wrap p-2 px-4 flex-col md:flex-row items-center">
         <a
           className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
           href="#test"
         >
-          <span className="ml-3 text-xl dark:text-white">InternetRadio.ge</span>
+          <span className="ml-3 text-xl dark:text-white font-black">
+            InternetRadio.ge
+          </span>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-200	flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 dark:text-white" href="#test">
-            First Link
+          <a
+            className="mr-5 dark:text-white hover:text-indigo-500 dark:hover:text-purple-500 font-bold"
+            href="#test"
+          >
+            Browse
           </a>
-          <a className="mr-5 dark:text-white" href="#test">
-            Second Link
+          <a
+            className="mr-5 dark:text-white hover:text-indigo-500 dark:hover:text-purple-500 font-bold"
+            href="#test"
+          >
+            Discover
           </a>
-          <a className="mr-5 dark:text-white" href="#test">
-            Third Link
+          <a
+            className="mr-5 dark:text-white hover:text-indigo-500 dark:hover:text-purple-500 font-bold"
+            href="#test"
+          >
+            Podcasts
           </a>
-          <a className="mr-5 dark:text-white" href="#test">
-            Fourth Link
+          <a
+            className="mr-5 dark:text-white hover:text-indigo-500 dark:hover:text-purple-500 font-bold"
+            href="#test"
+          >
+            Other
           </a>
         </nav>
         <button
           onClick={() => setTheme(colorTheme)}
-          className="inline-flex items-center bg-gray-200 font-bold dark:bg-gray-600 dark:text-white border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0"
+          className="inline-flex items-center bg-gray-100 hover:bg-gray-200 font-bold dark:bg-gray-600 dark:text-white border-0 py-1 px-2 focus:outline-none rounded text-base mt-4 md:mt-0"
         >
-          {colorTheme === "light" ? "Dark" : "Light"}
+          {colorTheme === "light" ? (
+            <Sun width={20} height={20} />
+          ) : (
+            <Moon width={20} height={20} />
+          )}
         </button>
       </div>
     </header>
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './assets/tailwind.css'
+//react imports
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// css imports
+import "./assets/tailwind.css";
+import "./assets/main.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
