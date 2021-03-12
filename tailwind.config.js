ff36dda88c1694451430d6f180410715facdd374
Shevek <gtophuria@gmail.com>
2021-03-12 13:39:45 +0400

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.trueGray,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
