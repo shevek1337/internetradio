26efab160c1cb4af9b535224ee62aaa653853ae2
Shevek <gtophuria@gmail.com>
2021-03-13 01:55:55 +0400

    :lipstick: updated header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,36 +9,36 @@ const Header = () => {
   const [colorTheme, setTheme] = useDarkMode();
   return (
     <header className="dark:bg-black">
-      <div className="mx-auto flex flex-wrap p-2 px-4 flex-col md:flex-row items-center">
+      <div className="container mx-auto flex flex-wrap py-2 flex-col md:flex-row items-center">
         <a
           className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
           href="#test"
         >
-          <span className="ml-3 text-xl dark:text-white font-black">
+          <span className="text-xl dark:text-white font-black">
             InternetRadio.ge
           </span>
         </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-200	flex flex-wrap items-center text-base justify-center">
+        <nav className="md:mr-auto md:ml-8 md:pt-1 flex flex-wrap text-base space-x-4">
           <a
-            className="mr-5 dark:text-white hover:text-indigo-500 dark:hover:text-purple-500 font-bold"
+            className="dark:text-white hover:text-purple-700 dark:hover:text-purple-500 font-bold"
             href="#test"
           >
             Browse
           </a>
           <a
-            className="mr-5 dark:text-white hover:text-indigo-500 dark:hover:text-purple-500 font-bold"
+            className="dark:text-white hover:text-purple-700 dark:hover:text-purple-500 font-bold"
             href="#test"
           >
             Discover
           </a>
           <a
-            className="mr-5 dark:text-white hover:text-indigo-500 dark:hover:text-purple-500 font-bold"
+            className="dark:text-white hover:text-purple-700 dark:hover:text-purple-500 font-bold"
             href="#test"
           >
             Podcasts
           </a>
           <a
-            className="mr-5 dark:text-white hover:text-indigo-500 dark:hover:text-purple-500 font-bold"
+            className="dark:text-white hover:text-purple-700 dark:hover:text-purple-500 font-bold"
             href="#test"
           >
             Other
@@ -46,7 +46,7 @@ const Header = () => {
         </nav>
         <button
           onClick={() => setTheme(colorTheme)}
-          className="inline-flex items-center bg-gray-100 hover:bg-gray-200 font-bold dark:bg-gray-600 dark:text-white border-0 py-1 px-2 focus:outline-none rounded text-base mt-4 md:mt-0"
+          className="inline-flex items-center bg-gray-100 hover:bg-gray-200 font-bold dark:bg-gray-600 dark:text-white border-0 py-1 px-2 focus:outline-none rounded"
         >
           {colorTheme === "light" ? (
             <Sun width={20} height={20} />
